85976bf6e36e5af4b132c4f6042e2f1844965064
simonchan66 <135537683+simonchan66@users.noreply.github.com>
2023-06-22 20:25:52 -0600

    Add files via upload

---
+++ public/dice.js
@@ -0,0 +1,21 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+var img_1 = "./images/dice" + randomNumber1 + ".png";
+var img_2 = "./images/dice" + randomNumber2 + ".png";
+document.querySelector("img.img1").setAttribute("src", img_1);
+
+document.querySelector("img.img2").setAttribute("src", img_2);
+if (randomNumber1>randomNumber2){
+    document.querySelector("h1").textContent="Simon Wins";
+    
+}
+else if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").textContent="Fair Game";
+}
+else {
+    document.querySelector("h1").textContent="Carina Wins";
+}
+
+document.querySelector("#reload").addEventListener("click", function() {
+    location.reload();
+  });
